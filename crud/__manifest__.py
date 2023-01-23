c0a0ffd20fd8e25aadafcde39152cf83dbbde092
David Rodríguez <david.rodriguez@quadit.mx>
2023-01-23 13:16:52 -0600

    examen 23-ene-2023

---
+++ crud/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "crud",
+    'sumary': """Creación CRUD""",
+    'author': "David Rodríguez ",
+    'category': '',
+
+    'version': '1.0',
+    
+    'depends': ["account"],
+    
+    'data': {'views/vista.xml', 'security/ir.model.access.csv'},
+    'instalable':True,
+}
